5a89dbd5127184eeac32d380055d4f03d78c0eea
raven <183471726@qq.com>
2016-10-17 16:21:01 +0800

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: 'module'
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'comma': 0,
+    'spaced-comment': 0,
+    'comma-dangle': 0,
+    'no-new': 0,
+    'semi': 0,
+    'space-before-blocks': 0,
+    'space-before-function-paren': 0,
+    'camelcase': 0,
+    'quotes': 0,
+    'indent': 0,
+    'padded-blocks': 0
+
+  }
+}
